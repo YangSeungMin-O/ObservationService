3839f576ee5338936b8fbd6818629f60cc1caf8a
YangSeungMin <ysm6768@gmail.com>
2022-04-11 18:23:41 +0900

    차트JS, 브이월드 지도 테스트

--- src/main/webapp/WEB-INF/jsp/os/main/main.jsp
+++ src/main/webapp/WEB-INF/jsp/os/main/main.jsp
@@ -4,12 +4,41 @@
 <head>
 <meta charset="UTF-8">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.14.1/build/ol.js"></script>
-<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList"></script>
+<script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <script src="/js/os/common/common.js"></script>
 <script src="/js/os/main/main.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.4.3/css/ol.css">
 <title>관측서비스-메인페이지</title>
+<style>
+	#map {
+		height: 500px;
+		width : 30%;
+	}
+</style>
 </head>
 <body>
+	<h2>MainPage</h2>
+	<div id="map"></div>
+	<table border="1" style="text-align: center; height:100px; width:400px;">
+		<tr>
+			<td>GL</td>
+			<td>EL</td>
+			<td>전기전도도</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>1</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>1</td>
+			<td>1</td>
+		</tr>
+	</table>
+	<div style="position: relative; height:200px; width:400px;">
+		<canvas id="myChart"></canvas>
+	</div>
 </body>
 </html>
--- src/main/webapp/js/os/main/main.js
+++ src/main/webapp/js/os/main/main.js
@@ -1,7 +1,62 @@
-
-/* 이벤트성 함수 */
 $(function(){
-
+	createMap();
+	createChart();
 })
 
+function createMap(){
+	var map = new ol.Map({
+		target: 'map',
+		layers: [new ol.layer.Tile({
+					source: new ol.source.XYZ({
+						url: 'http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png'
+					})
+				})],
+		view: new ol.View({
+		center: [14126669.41589247, 4493404.190498611],
+		zoom: 6,
+		minZoom: 6,
+		maxZoom: 19
+		})
+	});
+}
 
+function createChart(){
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+	        datasets: [{
+	            label: 'Label',
+	            data: [12, 19, 3, 5, 2, 3],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        maintainAspectRatio: true,
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
