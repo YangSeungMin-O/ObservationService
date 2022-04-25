6c4f32102c1bcd835ca1f2d4be2a154b8ca06961
YangSeungMin <ysm6768@gmail.com>
2022-04-25 17:10:54 +0900

    배경지도, 위성영상, 하이브리드지도 전환 및 마커기능

--- src/main/webapp/WEB-INF/jsp/os/mainPage/mainPage.jsp
+++ src/main/webapp/WEB-INF/jsp/os/mainPage/mainPage.jsp
@@ -5,73 +5,14 @@
 <meta charset="UTF-8">
 <title>관측서비스-대시보드</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-<script src="/js/os/common/common.js"></script>
-<script src="/js/os/main/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.6.2/proj4.js"></script>
+<script type="text/javascript" src="/js/os/main/main.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.4.3/css/ol.css">
-<script type="text/javascript">
-	function createMap(){
-		var map = new ol.Map({
-			target: 'map',
-			layers: [new ol.layer.Tile({
-						source: new ol.source.XYZ({
-							url: 'http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png'
-						})
-					})],
-			view: new ol.View({
-			center: [14126669.41589247, 4493404.190498611],
-			zoom: 7,
-			minZoom: 7,
-			maxZoom: 19
-			})
-		});
-	}
-	function createChart(){
-		var ctx = document.getElementById("myChart").getContext('2d');
-		var myChart = new Chart(ctx, {
-		    type: 'bar',
-		    data: {
-		        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		        datasets: [{
-		            label: 'Label',
-		            data: [12, 19, 3, 5, 2, 3],
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.2)',
-		                'rgba(54, 162, 235, 0.2)',
-		                'rgba(255, 206, 86, 0.2)',
-		                'rgba(75, 192, 192, 0.2)',
-		                'rgba(153, 102, 255, 0.2)',
-		                'rgba(255, 159, 64, 0.2)'
-		            ],
-		            borderColor: [
-		                'rgba(255,99,132,1)',
-		                'rgba(54, 162, 235, 1)',
-		                'rgba(255, 206, 86, 1)',
-		                'rgba(75, 192, 192, 1)',
-		                'rgba(153, 102, 255, 1)',
-		                'rgba(255, 159, 64, 1)',
-		            ],
-		            borderWidth: 1
-		        }]
-		    },
-		    options: {
-		        maintainAspectRatio: true,
-		        scales: {
-		            yAxes: [{
-		                ticks: {
-		                    beginAtZero:true
-		                }
-		            }]
-		        }
-		    }
-		});
-	}
-</script>
 <style type="text/css">
 	#contentDiv {
 	}
@@ -80,29 +21,37 @@
 		width : 100%;
 	}
 	#status {
-		width:400px;
+		width:200px;
 		text-align: center;
+		margin-left: 10px;
+		margin-top: 10px;
 	}
 	.left {
-		width: 50%;
+		width: 70%;
 		height: 880px;
 		float: left;
 		box-sizing: border-box;
 		background: #D3D3D3;
 	}
 	.right {
-		width: 25%;
+		width: 15%;
 		height: 880px;
 		float: left;
 		box-sizing: border-box;
 		background: #D3D3D3;
 	}
 	.right2 {
-		width: 25%;
+		width: 15%;
 		height: 880px;
 		float: left;
 		box-sizing: border-box;
 		background: #D3D3D3;
+	}
+	.a {
+		height: 48px;
+	}
+	.form-check {
+		margin-left: 10px;
 	}
 </style>
 </head>
@@ -156,28 +105,27 @@
 		</div>
 		<!-- 오른쪽 컨텐츠 내용 -->
       	<div class="right">
-      		<h3>OO지점 측정데이터</h3>
-			<div id="chart" style="position: relative; height:200px; width:400px;">
-				<canvas id="myChart"></canvas>
+      		<table id="status" class="table table-striped table-dark">
+      			<tr class="a">
+      				<td>현재마우스 위치 좌표</td>
+      			</tr>
+      			<tr class="a">
+      				<td><div id="mouse-position"></div></td>
+      			</tr>
+      		</table>
+			<div class="form-check">
+				<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onclick="mapUpdate('vword')" checked>
+				<label class="form-check-label" for="flexRadioDefault1">배경지도</label>
 			</div>
-			<h3>지역별 측정소 상태정보</h3>
-			<table id="status" class="table table-striped table-dark">
-				<tr>
-					<td>지역</td>
-					<td>측정소 갯수</td>
-					<td>정상</td>
-					<td>점검중</td>
-					<td>이상</td>
-				</tr>
-				<tr>
-					<td>경기</td>
-					<td>100</td>
-					<td>80</td>
-					<td>10</td>
-					<td>10</td>
-				</tr>
-			</table>
-      	</div>
+			<div class="form-check">
+				<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onclick="mapUpdate('satellite')">
+				<label class="form-check-label" for="flexRadioDefault2">위성영상</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onclick="mapUpdate('hybrid')">
+				<label class="form-check-label" for="flexRadioDefault2">하이브리드 지도</label>
+			</div>
+   		</div>
       	<div class="right2">
       	</div>
     </div>
--- src/main/webapp/js/os/main/main.js
+++ src/main/webapp/js/os/main/main.js
@@ -1,62 +1,153 @@
 $(function(){
 	createMap();
-	createChart();
 })
 
-function createMap(){
-	var map = new ol.Map({
-		target: 'map',
-		layers: [new ol.layer.Tile({
-					source: new ol.source.XYZ({
-						url: 'http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png'
-					})
-				})],
-		view: new ol.View({
-		center: [14126669.41589247, 4493404.190498611],
-		zoom: 6,
-		minZoom: 6,
-		maxZoom: 19
-		})
-	});
+var map;
+var pointLocation = [];
+var rasterLayer =  new ol.layer.Tile({
+	source: new ol.source.XYZ({url: 'http://xdworld.vworld.kr:8080/2d/Base/201710/{z}/{x}/{y}.png'})
+})
+var hybridLayer =  new ol.layer.Tile({
+	source: new ol.source.XYZ({url: 'http://xdworld.vworld.kr:8080/2d/Hybrid/201612/{z}/{x}/{y}.png'})
+})
+var satelliteLayer =  new ol.layer.Tile({
+	source: new ol.source.XYZ({url: 'http://xdworld.vworld.kr:8080/2d/Satellite/201710/{z}/{x}/{y}.jpeg'})
+})
+
+/* 지도유형 변경 */
+function mapUpdate(option){	
+	map.removeLayer(rasterLayer);
+	map.removeLayer(hybridLayer);
+	map.removeLayer(satelliteLayer);
+	switch (option) {
+	case 'vword':
+		map.addLayer(rasterLayer);
+		markerView();
+		break;
+	case 'hybrid':
+		map.addLayer(satelliteLayer);
+		map.addLayer(hybridLayer);
+		markerView();
+		break;
+	case 'satellite':
+		map.addLayer(satelliteLayer);
+		markerView();
+		break;
+	case undefined:
+		map.addLayer(rasterLayer);
+		markerView();
+		break;
+	}
 }
 
-function createChart(){
-	var ctx = document.getElementById("myChart").getContext('2d');
-	var myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	        datasets: [{
-	            label: 'Label',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)',
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        maintainAspectRatio: true,
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
+/* 마커현행유지 */
+function markerView(){
+	console.log(pointLocation.length);
+	console.log(pointLocation);
+	for (var i=0; i<pointLocation.length; i++) {
+		addMarker2(pointLocation[i]);
+	}
+}
+
+/* 지도 모듈 */
+function createMap(){
+	/* 벡터레이어 */
+	var vectorLayer = new ol.layer.Vector({
+		source: new ol.source.Vector()
 	});
+
+	var mousePositionCtrl = new ol.control.MousePosition({
+		  coordinateFormat: ol.coordinate.createStringXY(4),
+		  projection: 'EPSG:4326',
+		  className: 'custom-mouse-position',
+		  target: document.getElementById('mouse-position'),
+		  undefinedHTML: '&nbsp;'
+		});
+	
+	map = new ol.Map({
+		/* map DIV가 타겟 */
+		target: 'map',
+		controls: ol.control.defaults().extend( [mousePositionCtrl] ),
+		layers: [new ol.layer.Tile({
+					source: new ol.source.XYZ({
+						/* vWorld API 사용 */
+						url: 'http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png'
+					})
+					}),
+					vectorLayer
+				],
+		view: new ol.View({
+	        center: new ol.geom.Point([ 126.97659953, 37.579220423 ])
+	      	/* GPS 좌표계 -> 구글 좌표계 */
+	        .transform('EPSG:4326', 'EPSG:3857')
+	        /* 포인트의 좌표를 리턴 */
+        	.getCoordinates(),
+			zoom: 7,
+			minZoom: 7,
+			maxZoom: 19
+		})
+	});
+	
+	/* 클릭한 지도위치 좌표 반환(배열) */
+	map.on('click', function(evt) {
+	    var coordinate = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+	    console.log(coordinate);
+	    /* 클릭한 좌표로 마커 생성 */
+	    addMarker(coordinate);
+	})
+}
+
+/* 마커생성 모듈 */
+function addMarker(coordinate){
+	pointLocation.push(coordinate);
+    var feature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([coordinate[0], coordinate[1]])) //경도 위도에 포인트 설정
+    });
+
+    /* 마커스타일 설정 */
+    var markerStyle = new ol.style.Style({
+        image: new ol.style.Icon({ //마커 이미지
+        	opacity: 1,
+        	scale: 1.2,
+            src: '/images/marker3.png'
+        })
+    });
+
+    var markerSource = new ol.source.Vector({
+        features: [feature]
+    });
+
+    var markerLayer = new ol.layer.Vector({
+        source: markerSource, 
+        style: markerStyle 
+    });
+    /* 지도에 마커가 그려진 레이어 추가 */
+    map.addLayer(markerLayer);
+}
+
+/* 마커현행유지 위한 마커생성 모듈 */
+function addMarker2(coordinate){
+    var feature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([coordinate[0], coordinate[1]])) //경도 위도에 포인트 설정
+    });
+
+    /* 마커스타일 설정 */
+    var markerStyle = new ol.style.Style({
+        image: new ol.style.Icon({ //마커 이미지
+        	opacity: 1,
+        	scale: 1.2,
+            src: '/images/marker3.png'
+        })
+    });
+
+    var markerSource = new ol.source.Vector({
+        features: [feature]
+    });
+
+    var markerLayer = new ol.layer.Vector({
+        source: markerSource, 
+        style: markerStyle 
+    });
+    /* 지도에 마커가 그려진 레이어 추가 */
+    map.addLayer(markerLayer);
 }
